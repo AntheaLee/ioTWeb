0e8a4f311bbf8ee2cc7ab433fa32b6c7f93d6f1f
AntheaLee <940393@gmail.com>
2021-10-02 16:22:24 +0800

    Add files via upload

--- getXSRF.js
+++ getXSRF.js
@@ -4,7 +4,7 @@
 axios.get('http://10.118.126.210/sanctum/csrf-cookie')
   .then(function (response) {
     // handle success
-    console.log("success");
+    console.log("success get cookie");
 
     console.log(response);
   })
@@ -17,33 +17,38 @@ axios.get('http://10.118.126.210/sanctum/csrf-cookie')
     // always executed
   });
 
-  console.log("done www");
-let data = {
-    "email": "testtest@gmail.com",
-    "password": "test",
-    "remember": '1'
-};
+  console.log("get cookie done");
 
-//請求表頭
-let header = {
+
+
+$(function () { $('#signin').click(function () {
+  console.log("click sign in");
+
+  let data = {
+    "email": $('#inputEmail').val(),
+    "password": password = $('#inputPassword').val(),
+    "remember": '1'
+  };
+
+  // Request headers
+  let header = {
     "Content-Type": "application/json",
     "Accept": "application/json",
     "X-XSRF-TOKEN": $('meta[name="xsrf-token"]').attr('content')
-   // "X-XSRF-TOKEN":""
+    // "X-XSRF-TOKEN":""
     //"Origin":"http://10.118.126.210"
     //"Authorization": `Bearer ${token}`,
     
 };
-
-console.log(document.cookie);
-console.log("pleassssss!");
- axios({
-     method: 'post',
-     url: `http://10.118.126.210/api/auth/login`,
-     data: data,
-     headers: header,
-     withCredentials: true
-    
- })
-     .then( (response) => console.log(response))
-     .catch( (error) => console.log(error))
+    axios({
+        method: 'post',
+        url: `http://10.118.126.210/api/auth/login`,
+        data: data,
+        headers: header,
+        withCredentials: true
+        
+    })
+        .then( (response) => console.log(response))
+        .catch( (error) => console.log(error))
+}); 
+}); 

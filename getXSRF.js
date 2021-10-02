6c47eb98cf04d5df279ea85c1903a97764b514b5
Gitea <gitea@fake.local>
2021-10-02 15:59:05 +0800

    login success

--- getXSRF.js
+++ getXSRF.js
@@ -1,7 +1,7 @@
 
 // Make a request for a user with a given ID
 
-axios.get('http://10.118.126.210:8080/sanctum/csrf-cookie')
+axios.get('http://10.118.126.210/sanctum/csrf-cookie')
   .then(function (response) {
     // handle success
     console.log("success");
@@ -19,28 +19,31 @@ axios.get('http://10.118.126.210:8080/sanctum/csrf-cookie')
 
   console.log("done www");
 let data = {
-    "email": "888@gmail.com",
-    "password": "888",
+    "email": "testtest@gmail.com",
+    "password": "test",
     "remember": '1'
-}
+};
 
 //請求表頭
 let header = {
     "Content-Type": "application/json",
     "Accept": "application/json",
-    "X-XSRF-TOKEN":""
+    "X-XSRF-TOKEN": $('meta[name="xsrf-token"]').attr('content')
+   // "X-XSRF-TOKEN":""
     //"Origin":"http://10.118.126.210"
     //"Authorization": `Bearer ${token}`,
     
 };
 
-// axios({
-//     method: 'post',
-//     url: `http://10.118.126.210:8080/api/auth/login`,
-//     data: data,
-//     headers: header,
-//     withCredentials: true
+console.log(document.cookie);
+console.log("pleassssss!");
+ axios({
+     method: 'post',
+     url: `http://10.118.126.210/api/auth/login`,
+     data: data,
+     headers: header,
+     withCredentials: true
     
-// })
-//     .then( (response) => console.log(response))
-//     .catch( (error) => console.log(error))
+ })
+     .then( (response) => console.log(response))
+     .catch( (error) => console.log(error))

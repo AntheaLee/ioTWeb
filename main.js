dd31b42315226ec46dd909474895ee32347bdd83
AntheaLee <940393@gmail.com>
2021-10-03 20:56:18 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -22,14 +22,38 @@ function isMtpFile(){
   } 
 }
 
-$(function () {
-  $('#fileupload').fileupload({
-      dataType: 'json',
-      done: function (e, data) {
-          $.each(data.result.files, function (index, file) {
-              $('<p></p>').text(file.name).appendTo(document.body);
-          });
-      }
+// $(function () {
+//   $('#fileupload').fileupload({
+//       dataType: 'json',
+//       done: function (e, data) {
+//           $.each(data.result.files, function (index, file) {
+//               $('<p></p>').text(file.name).appendTo(document.body);
+//           });
+//       }
+//   });
+// });
+
+
+var files = [];
+$(document).ready(function(){
+  $("input").change(function(){
+    files = this.files;
   });
 });
-
+$("#upload-btn").click(function(){
+  var fd = new FormData();
+  for (var i = 0; i < files.length; i++) {
+    fd.append("file", files[i]);
+  }
+  $.ajax({
+    url: "10.118.126.210/api/files",
+    method: "POST",
+    data: fd,
+    contentType: false,
+    processData: false,
+    cache: false,
+    success: function(data){
+      console.log(data);
+    }
+  });
+});
